db5ed61a1868576b070c1b5e0e5009cf88b28df0
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-12-27 22:03:35 +0530

    init
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <div>
+
+        <p>Apequest</p>
+
+        <a href="https://app.apequest.club" target="_blank">App</a>
+        <br />
+        <a href="https://app.apequest.club/admin" target="_blank">Admin</a>
+      </div>
     </div>
   );
 }
